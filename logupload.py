8f35ac159df546074edbdd11d8267d1e4e2fe791
jdavemariano <143034233+jdavemariano@users.noreply.github.com>
2023-11-26 01:16:13 +0800

    Update logupload.py

--- logupload.py
+++ logupload.py
@@ -8,11 +8,12 @@ def upload_to_s3(file_path, bucket_name):
     file_name, file_extension = os.path.splitext(os.path.basename(file_path))
     s3_object_key = f'{file_name}_{target_date}{file_extension}'
 
-        try: 
-            s3.upload_file(file_path, bucket_name, s3_object_key)
-            print(f'Succefully uploaded {file_path} to {bucket_name}/{s3_object_key}')
-        except Exception as e:
-            print (f' Error uploading {file_path} to {bucket_name}')
+    try: 
+        s3.upload_file(file_path, bucket_name, s3_object_key)
+        print(f'Succefully uploaded {file_path} to {bucket_name}/{s3_object_key}')
+    except Exception as e:
+        print (f' Error uploading {file_path} to {bucket_name}')
+        
 if __name__ = '__main__':
     file_path = "command_id_output.txt"
     bucket_name = "ad-user-cutoff-update"
